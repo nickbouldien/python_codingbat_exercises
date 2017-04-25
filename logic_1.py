26e16803defef4677af2e0aa179f543eaf38d7ec
nick bouldien <nick.bouldien@gmail.com>
2017-04-24 23:12:56 -0700

    logic puzzles in python, initial commit

---
+++ logic_1.py
@@ -0,0 +1,61 @@
+
+
+def cigar_party(cigars, is_weekend):
+    if is_weekend == True:
+        if cigars >= 40:
+            return True
+        else:
+            return False
+    else:
+        if (cigars >=40 and cigars <=60):
+            return True
+        else:
+            return False
+
+
+def date_fashion(you, date):
+if you <=2  or date <= 2:
+if you >= 8 or date >= 8:
+    return 2
+else:
+    return 0
+elif you >=8 or date >= 8:
+return 2
+else:
+return 1
+
+
+def squirrel_play(temp, is_summer):
+    if is_summer == True:
+        if temp >= 60 and temp <= 100:
+            return True
+        else:
+            return False
+    else:
+        if temp >= 60 and temp <= 90:
+            return True
+        else:
+            return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
