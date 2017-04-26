6fb20fb9e07862515ecc92714edb44e4e71cf4d5
nick bouldien <nick.bouldien@gmail.com>
2017-04-25 23:17:35 -0700

    logic puzzles1 update. one remaining problem

--- logic_1.py
+++ logic_1.py
@@ -14,15 +14,15 @@ def cigar_party(cigars, is_weekend):
 
 
 def date_fashion(you, date):
-if you <=2  or date <= 2:
-if you >= 8 or date >= 8:
-    return 2
-else:
-    return 0
-elif you >=8 or date >= 8:
-return 2
-else:
-return 1
+    if you <=2  or date <= 2:
+        # if you >= 8 or date >= 8:
+        #     return 2
+        # else:
+            return 0
+    elif you >=8 or date >= 8:
+        return 2
+    else:
+        return 1
 
 
 def squirrel_play(temp, is_summer):
@@ -38,19 +38,75 @@ def squirrel_play(temp, is_summer):
             return False
 
 
+def caught_speeding(speed, is_birthday):
+    if is_birthday == True:
+        if speed >=66 and speed <=85:
+            return 1
+        elif speed <= 65:
+            return 0
+        else:
+            return 2
+    else:
+        if speed >=61 and speed <=80:
+            return 1
+        elif speed <= 60:
+            return 0
+        else:
+            return 2
+
+
+def sorta_sum(a, b):
+    if (a + b) >= 10 and (a + b) <= 19:
+        return 20
+    else:
+        return (a + b)
+
+
+def alarm_clock(day, vacation):
+    if vacation == True:
+        if day in range(1,6):
+            return "10:00"
+        else: # sab/dom
+            return "off"
+    else: # not on vacation
+        if day in range(1,6):
+            return "7:00"
+        else: # sab/dom
+            return "10:00"
 
 
 
+def in1to10(n, outside_mode):
+    if outside_mode == True:
+        if n not in range(2,10):
+            return True
+        else:
+            return False
+    else:
+        if n in range(1,11):
+            return True
+        else:
+            return False
 
 
+def near_ten(num):
+    if num == 0:
+        return True
+    else:
+        if ( num % 10 < 2 or num % 2 < 2 or num % 5 < 2 ):
+            return True
+        else:
+            return False
 
 
-
-
-
-
-
-
+def love6(a, b):
+    if a == 6 or b == 6:
+        return True
+    elif abs(a - b) == 6 or abs(b-a) == 6:
+        return True
+    elif abs(a + b) == 6 or abs(b+a) == 6:
+    else:
+        return False
 
 
 
